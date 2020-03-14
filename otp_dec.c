7a9964729696f8afe493629bddb84c7c015ad343
Gerson Lindor Jr <lindorg@oregonstate.edu>
2020-03-13 18:13:08 -0700

    having issues sending plaintext data from otp_dec_d

---
+++ otp_dec.c
@@ -0,0 +1,81 @@
+/* filename: otp_dec.c
+ *
+ * SYNOPSIS:
+ *
+ * DESCRIPTION:
+ *
+ * AUTHOR: Gerson Lindor Jr. (lindorg@oregonstate.edu)
+ * DATE CREATED: March 13, 2020
+ * DATE LAST MODIFIED:
+ */
+
+
+#include "client.h"
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int main(int argc, char **argv) {
+    struct clientServer *enc = getClient();
+    char mykey[STR_MAX];
+    char plaintext[STR_MAX];
+    char ciphertext[STR_MAX];
+    char serverName[10];
+    int textSize = -1;
+    char strSize[6];
+
+    memset(serverName, '\0', 10);
+    memset(strSize, '\0', 6);
+    memset(plaintext, '\0', STR_MAX);
+    memset(ciphertext, '\0', STR_MAX);
+    memset(mykey, '\0', STR_MAX);
+    // get the plaintext from file
+    readFile(argv[1], ciphertext);
+    // get the key from file
+    readFile(argv[2], mykey);
+    // verify command line inputs
+    if (checkInput(ciphertext, mykey, argv[3], argc)) {
+        // setup the server address struct
+        setUpAddress(enc, argv[3], "localhost");
+        // setup the socket and connect to the server
+        setUpConnect(enc);
+        // receive the size of incomming text
+        textSize = recvSize(enc);
+        // receive the name of the server, and verify its otp_enc_d
+        recvMsg(enc, serverName, textSize);
+        if (strcmp(serverName, "otp_dec_d") != 0) {
+            fprintf(stderr, "Error could not contact otp_dec_d on port %d\n", getPortNumber(enc));
+            closeClient(enc);
+            exit(2);
+        }
+// printf("--------- NOW SENDING MESSAGES --------\n");
+        // send key size
+        textSize = strlen(mykey);
+///        changeToString(textSize, strSize, 6);
+//printf("client send key size:\n"); 
+        sendSize(enc, textSize);
+//printf("client send key size = %d\n", textSize); 
+        // send the key
+        sendMsg(enc, mykey);
+        // send ciphertext size
+        textSize = strlen(ciphertext);
+////        changeToString(textSize, strSize, 6);
+//printf("client send plaintext size:\n"); 
+        sendSize(enc, textSize);
+///printf("client send plaintext size = %d\n", textSize); 
+        // send the ciphertext
+        sendMsg(enc, ciphertext);
+        // receive the size of the plaintext
+        textSize = recvSize(enc);
+printf("Client received plaintext size = %d\n", textSize);
+        // receive the plaintext
+        recvMsg(enc, plaintext, textSize);
+        // close the socket
+        closeClient(enc);
+    }
+    printf("%s\n", plaintext);
+    if (enc) { free(enc); enc = NULL; }
+    return 0;
+}
+
--- otp_dec_d.c
+++ otp_dec_d.c
@@ -1,11 +1,11 @@
-/* filename: otp_enc_d.c
+/* filename: otp_dec_d.c
  *
  * SYNOPSIS:
  *
  * DESCRIPTION:
  *
  * AUTHOR: Gerson Lindor Jr. (lindorg@oregonstate.edu)
- * DATE CREATED: March 5, 2020
+ * DATE CREATED: March 13, 2020
  * DATE LAST MODIFIED:
  */
 
@@ -38,38 +38,39 @@ int main(int argc, char **argv) {
             // accept a connection, blocking if one is not available until one connects
             if (acceptConnection(encD)) {
                 memset(mykey, '\0', MAX);
-                memset(plaintext, '\0', MAX);
+                memset(ciphertext, '\0', MAX);
                 memset(strSize, '\0', 6);
                 // send size of server name
-                textSize = strlen("otp_enc_d");
+                textSize = strlen("otp_dec_d");
  ///               changeToString(textSize, strSize, 6);
                 sendSize(encD, textSize);
+printf("Server sends otp_dec_d size = %d\n", textSize);
                 // send server name to client
-                sendMsg(encD, "otp_enc_d");
+                sendMsg(encD, "otp_dec_d");
 // printf("--------- NOW RECEIVING MESSAGES FROM CLIENT--------\n");
                 // receive size of key
                 textSize = recvSize(encD);
 //    printf("server received key size = %d\n", textSize); 
-                // receive the key from otp_enc
+                // receive the key from otp_dec
                 recvMsg(encD, mykey, textSize);
-                // receive size from plaintext
+                // receive size from ciphertext
                 textSize = recvSize(encD);
 //    printf("server received plaintext size = %d\n", textSize); 
-                // receive the plaintext from otp_enc
-                recvMsg(encD, plaintext, textSize);
-                // encrypt the data
-                memset(ciphertext, '\0', MAX);
-                secureTransfer(secretMap, mapSize, plaintext, mykey, ciphertext, 0);
-                // send size of ciphertext
+                // receive the ciphertext from otp_dec
+                recvMsg(encD, ciphertext, textSize);
+                // decrypt the data
+                memset(plaintext, '\0', MAX);
+                secureTransfer(secretMap, mapSize, ciphertext, mykey, plaintext, 1);
+                // send size of plaintext
 //                do {
-                textSize = strlen(ciphertext);
+                textSize = strlen(plaintext);
 ////                changeToString(textSize, strSize, 6);
 //printf("Server sends ciphertext size:\n");
                 sendSize(encD, textSize);
 //                } while (textSize = 0);
-//printf("Server sends ciphertext size = %d\n", textSize);
-                // send ciphertext to otp_enc
-                sendMsg(encD, ciphertext);
+printf("Server sends plaintext size = %d\n", textSize);
+                // send plaintext to otp_enc
+                sendMsg(encD, plaintext);
                 // close established connection with the client
                 disconnect(encD);
             }
